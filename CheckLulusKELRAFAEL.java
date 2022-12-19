67fc1b311791787bab32b83694ff7ba2b179ade9
JeremyChristianB <118141660+JeremyChristianB@users.noreply.github.com>
2022-12-19 14:22:12 +0700

    Update CheckLulusKELRAFAEL.java

--- CheckLulusKELRAFAEL.java
+++ CheckLulusKELRAFAEL.java
@@ -14,89 +14,107 @@
  * @author hp
  */
 import java.util.Scanner;
-public class CheckLulusKELRAFAEL{
+import static org.junit.Assert.assertEquals;
+
+public class TestingLulusKelRaf{
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner sc = new Scanner(System.in);
-        int num = 0;
-        int Jadwal_MataKuliah;
-        int Nilai_MataKuliah;     
-        while(num <= 1){       
-        System.out.println("Silahkan pilih mata kuliah:");
-        System.out.println("1. Dasar-dasar Pemograman");
-        System.out.println("2. Analisis Struktur Data");
-        System.out.println("3. Pemograman Berbasis Web");
-        System.out.println("4. Pemograman Pada Perangkat Lunak");
-        System.out.println("5. Rekayasa Perangkat Lunak");
-        Jadwal_MataKuliah=sc.nextInt () ; 
-        switch(Jadwal_MataKuliah){
+//        Scanner sc = new Scanner(System.in);
+//        int num = 0;
+//        int Jadwal_MataKuliah;
+//        int Nilai_MataKuliah;     
+//        while(num <= 1){       
+//        System.out.println("Silahkan pilih mata kuliah:");
+//        System.out.println("1. Dasar-dasar Pemograman");
+//        System.out.println("2. Analisis Struktur Data");
+//        System.out.println("3. Pemograman Berbasis Web");
+//        System.out.println("4. Pemograman Pada Perangkat Lunak");
+//        System.out.println("5. Rekayasa Perangkat Lunak");
+//        Jadwal_MataKuliah = sc.nextInt () ; 
+//        switch(Jadwal_MataKuliah){
+//            case 1:
+//                System.out.println("Minimun nilai B");
+//                break;
+//            case 2:
+//                System.out.println("Minimun nilai B");
+//                break;
+//            case 3:
+//                System.out.println("Minimun nilai B");
+//                break;
+//            case 4:
+//                System.out.println("Minimun nilai B");
+//                break;
+//            case 5:
+//                System.out.println("Minimun nilai B");
+//                break;
+//            default:
+//                System.out.println("Silahkan Pilih 1,2,3,4,5");           
+//            }
+//        System.out.println("Silahkan Masukan Nilai:");
+//        System.out.println("1. A");
+//        System.out.println("2. A-");
+//        System.out.println("3. B+");
+//        System.out.println("4. B");
+//        System.out.println("5. B-");
+//        System.out.println("6. C+");
+//        System.out.println("7. C");
+//        System.out.println("8. D");
+//        System.out.println("9. E");        
+//        Nilai_MataKuliah=sc.nextInt();
+//            
+//        num++;
+//        }
+//        
+        assertEquals("Lulus",TestingLulusKelRaf.cekLulus(1));
+        assertEquals("Lulus",TestingLulusKelRaf.cekLulus(2));
+        assertEquals("Lulus",TestingLulusKelRaf.cekLulus(3));
+        assertEquals("Lulus",TestingLulusKelRaf.cekLulus(4));
+        assertEquals("Tidak Lulus",TestingLulusKelRaf.cekLulus(5));
+        assertEquals("Tidak Lulus",TestingLulusKelRaf.cekLulus(6));
+        assertEquals("Tidak Lulus",TestingLulusKelRaf.cekLulus(7));
+        assertEquals("Tidak Lulus",TestingLulusKelRaf.cekLulus(8));
+        assertEquals("Tidak Lulus",TestingLulusKelRaf.cekLulus(9));
+    }
+    
+    public static String cekLulus (int nilaiMatkul)
+    {
+        String res = "";
+        switch(nilaiMatkul){
             case 1:
-                System.out.println("Minimun nilai B");
+                res = "Lulus";
                 break;
             case 2:
-                System.out.println("Minimun nilai B");
+                res = "Lulus";
                 break;
             case 3:
-                System.out.println("Minimun nilai B");
+                res = "Lulus";
                 break;
             case 4:
-                System.out.println("Minimun nilai B");
+                res = "Lulus";
                 break;
             case 5:
-                System.out.println("Minimun nilai B");
-                break;
-            default:
-                System.out.println("Silahkan Pilih 1,2,3,4,5");           
-            }
-        System.out.println("Silahkan Masukan Nilai:");
-        System.out.println("1. A");
-        System.out.println("2. A-");
-        System.out.println("3. B+");
-        System.out.println("4. B");
-        System.out.println("5. B-");
-        System.out.println("6. C+");
-        System.out.println("7. C");
-        System.out.println("8. D");
-        System.out.println("9. E");        
-        Nilai_MataKuliah=sc.nextInt();
-        switch(Nilai_MataKuliah){
-            case 1:
-                System.out.println("Lulus");
-                break;
-            case 2:
-                System.out.println("Lulus");
-                break;
-            case 3:
-                System.out.println("Lulus");
-                break;
-            case 4:
-                System.out.println("Lulus");
-                break;
-            case 5:
-                System.out.println("Tidak Lulus");
+                res = "Tidak Lulus";
                 break;
             case 6:
-                System.out.println("Tidak Lulus");
+                res = "Tidak Lulus";
                 break;
             case 7:
-                System.out.println("Tidak Lulus");
+                res = "Tidak Lulus";
                 break;
             case 8:
-                System.out.println("Tidak Lulus");
+                res = "Tidak Lulus";
                 break;
             case 9:
-                System.out.println("Tidak Lulus");
+                res = "Tidak Lulus";
                 break;    
             default:
-                System.out.println("Silahkan Pilih 1,2,3,4,5,6,7,8,9");
+                res = "Silahkan Pilih 1,2,3,4,5,6,7,8,9";
                 break;
-        }        
-        num++;
-        }
+        }  
+        return res;
     }
 }
-
